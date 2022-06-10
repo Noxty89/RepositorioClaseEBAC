5ed9ceb390af9d97548dad8ef90112960e40fd97
Noxty <eleazaargarcia@outlook.com>
2022-06-10 01:31:49 -0500

    Tarea Modulo 11
    Tarea del modulo 11 terminada

--- MiPrimerProyectoUnity/Assets/Scenes/scripts/EjerciciosEstructuras.cs
+++ MiPrimerProyectoUnity/Assets/Scenes/scripts/EjerciciosEstructuras.cs
@@ -5,87 +5,122 @@ using UnityEngine;
 public class EjerciciosEstructuras : MonoBehaviour
 {
     public List<int> milista = new List<int>();
-
-    // Start is called before the first frame update
-
-     void Start()
+    public HashSet<int> miListaHash = new HashSet<int>();
+    Queue<string> miQueue = new Queue<string>();
+    Stack<string> mistack = new Stack<string>();
+    void Start()
     {
-        for (int i = 0; i < 5; i++) 
-        {
-            milista.Add(Random.Range(1, 20));
-
-        }
-        foreach (var num in milista)
-        {
-            Debug.Log(num);
-        }
-        milista.Sort();
-        foreach (var num in milista)
-        {
-            Debug.Log("lista ordenada" + " " + num);
-        }
-        milista.Reverse();
-        foreach (var num in milista)
-        {
-            Debug.Log("lista en reversa" + " " + num);
-        }
     }
-
     // Update is called once per frame
     void Update()
     {
 
     }
-        public void punto1()
-        {
-        ////        Crea una función que reciba como parámetros tres números, llene una lista de
-        ////números aleatorios del tamaño del primer número y los rangos de los números
-        ////aleatorios estén dados por el segundo y tercer parámetro, la firma de la función
-        ////será algo similar a: public List<int> miFuncion(int tamaño, int rangoInferior, int
-        ////rangoSuperior)
-        //int primerParamtero = 10;
-        //int segundoParametro = 1;
-        //int tercerParametro = 5;
-
-        //for (int i = 0; i <= primerParamtero; i++)
-        //{
-        //    milista.Add(Random.Range(tercerParametro, segundoParametro));
-        //    i++;
-
-        //}
-        //foreach (var numero in milista)
-        //{
-        //    Debug.Log(numero);
-        //}
-
-    }
-    public void punto2()
+    //    //Crea una función que reciba como parámetros tres números, llene una lista de
+    //    //números aleatorios del tamaño del primer número y los rangos de los números
+    //    //aleatorios estén dados por el segundo y tercer parámetro, la firma de la función
+    //    //será algo similar a: public List<int> miFuncion(int tamaño, int rangoInferior, int
+    //    //rangoSuperior)
+    public void punto1(int primerParametro, int segundoParametro, int tercerParametro)
     {
-        //        Crea una función que reciba como entrada un arreglo de enteros, y regrese un
-        //arreglo con los mismos números pero ordenados de manera descendente.
+        punto1(10, 5, 10);
+
+        for (int i = 0; i <= primerParametro; i++)
+        {
+
+            milista.Add(Random.Range(tercerParametro, segundoParametro));
+            i++;
+
+        }
+        foreach (var numero in milista)
+        {
+            Debug.Log(numero);
+        }
+    }
+    //Crea una función que reciba como entrada un arreglo de enteros, y regrese un
+    //arreglo con los mismos números pero ordenados de manera descendente.
+    public List<int> punto2()
+    {
+
         for (int i = 0; i < 5; i++)
         {
             milista.Add(Random.Range(1, 20));
-           
+
         }
-        foreach(var num in milista)
+        foreach (var num in milista)
         {
             Debug.Log(num);
         }
-        //aqui juge un poco con querrylinq.
-        milista.Reverse();
-        foreach (var num in milista)
-        {
-            Debug.Log("lista en reversa" + " " + num);
-        }
-
         milista.Sort();
         foreach (var num in milista)
         {
             Debug.Log("lista ordenada" + " " + num);
         }
-
+        milista.Reverse();
+        foreach (var num in milista)
+        {
+            Debug.Log("lista en reversa" + " " + num);
+        }
+        return (milista);
     }
-        
-    
+    ////    //Crea una función que reciba una lista previamente llena de números o strings
+    ////    //que contenga elementos repetidos, y de salida regresa un hashset sin elementos
+    ////    //repetidos, puedes usar la función contains o linq para simplificar esto.
+    public HashSet<int> punto3()
+    {
+        List<int> listaconrepetidos = new List<int>();
+        for (int i = 0; i <= 10; i++)
+            listaconrepetidos.Add(i);
+
+        var listaSinRepetidos = new HashSet<int>(listaconrepetidos);
+        foreach (var num in listaSinRepetidos)
+        {
+            Debug.Log(num);
+        }
+        return (listaSinRepetidos);
+    }
+    //Crea una función que reciba una pila de strings, imprime sus contenidos y llena
+    //con ellos una cola de strings y ahora imprime los elementos de la cola utilizando
+    //los métodos respectivos de cada estructura(push, pop, queue, dequeue y peek), 
+    //puedes copiar los elementos de la pila a la cola en el mismo uso del peek, esta
+    //función no retornará nada, solo imprimirá los valores en la consola.
+    public void queueDqueuePeek()
+    {
+        miQueue.Enqueue("aldeano 1");
+        miQueue.Enqueue("aldeano 2");
+        miQueue.Enqueue("aldeano 3");
+        miQueue.Enqueue("aldeano 4");
+        miQueue.Enqueue("aldeano 5");
+        Debug.Log(miQueue.Peek());
+        miQueue.Dequeue();
+        Debug.Log(miQueue.Peek());
+        miQueue.Dequeue();
+        Debug.Log(miQueue.Peek());
+        miQueue.Dequeue();
+        Debug.Log(miQueue.Peek());
+        miQueue.Dequeue();
+    }
+    public void pushPop()
+    {
+        mistack.Push("municion 1");
+        mistack.Push("municion 2");
+        mistack.Push("municion 3");
+        mistack.Push("municion 4");
+        mistack.Push("municion 5");
+        mistack.Push("municion 6");
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+        Debug.Log(mistack.Peek());
+        mistack.Pop();
+    }
+
 }
+
